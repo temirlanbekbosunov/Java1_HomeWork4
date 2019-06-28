90392966f5f8a536a56b3fa9fea81b6eb4d314d0
temirlanbekbosunov <temirlan.bekbosunov@gmail.com>
2019-06-28 16:35:50 +0600

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,25 +8,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String person[] = new String[3];
+        person = Arrays.copyOf(person, person.length + 1);
 
-        for (int i=0;i<person.length;i++) {
+        for (int i = 0; i < person.length; i++) {
             System.out.print("Введите имя человека: ");
-            person[i]= in.nextLine();
+            person[i] = in.nextLine();
         }
 
-        for (int i=0;i<=person.length;i++){
-        switch (i){
-            case 0:
-                System.out.println("Доброе утро "+person[i]);
-                break;
-            case 1:
-                System.out.println("Добрый день "+person[i]);
-                break;
-            case 2:
-                System.out.println("Добрый вечер "+person[i]);
-                break;
+        for (int i = 0; i <= person.length; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("Доброе утро " + person[i]);
+                    break;
+                case 1:
+                    System.out.println("Добрый день " + person[i]);
+                    break;
+                case 2:
+                    System.out.println("Добрый вечер " + person[i]);
+                    break;
+                case 3:
+                    System.out.println("Доброй ночи " + person[i]);
 
-        }
+            }
         }
 
     }
